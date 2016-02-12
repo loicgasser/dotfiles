73de9fabbee1022a1835bf1654b9b6b916b04bdd
loicgasser <loicgasser4@gmail.com>
2016-02-12 10:11:40 +0000

    Add empty .pgpass on setup

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ cp -f .gitconfig ~/
 cp -f .tmux.conf ~/
 cp -f .vimrc ~/
 cp -f .bash_profile ~/
+touch ~/.pgpass
+chmod go-rwx ~/.pgpass
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
--- setup_jessie.sh
+++ setup_jessie.sh
@@ -6,6 +6,6 @@ apt-get install software-properties-common curl
 curl -sL https://deb.nodesource.com/setup_5.x | bash -
 apt-get install --yes nodejs
 apt-get install --yes build-essential
-apt-get install npm nodejs-legacy openjdk-7-jre gcc+ g++ htop tmux git python-virtualenv libpq-dev libgeos-dev python-dev libproj-dev make gdal-bin python-gdal gettext subversion-tools
+apt-get install npm nodejs-legacy openjdk-7-jre gcc+ g++ htop tmux git python-virtualenv libpq-dev libgeos-dev python-dev libproj-dev libjpeg-dev libxml2-dev make gdal-bin python-gdal gettext subversion-tools htop
 apt-get install ruby ruby-dev
 gem install travis -v 1.8.2 --no-rdoc --no-ri
