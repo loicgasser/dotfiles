dd21517a159eddac9b51e3135ecb5167451ad5e3
loicgasser <loicgasser4@gmail.com>
2016-05-30 14:09:48 +0200

    Fix unsecure connection

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ cp -f .vimrc ~/
 cp -f .bash_profile ~/
 touch ~/.pgpass
 chmod go-rwx ~/.pgpass
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl -k https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
