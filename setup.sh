05129139384f595ed4b871741141c689228cb8a0
loicgasser <loicgasser4@gmail.com>
2016-08-23 08:57:56 +0000

    Combine bashrc files

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ cp -f .gitconfig ~/
 cp -f .tmux.conf ~/
 cp -f .vimrc ~/
 cp -f .bash_profile ~/
-cp -f .bashrc ~/
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/bashrc
+    cat ~/bashrc .bashrc > ~/.bashrc
+    rm -f ~/bashrc
+else
+    cp -f .bashrc ~/.bashrc
+fi
 touch ~/.pgpass
 chmod go-rwx ~/.pgpass
 curl -k https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
