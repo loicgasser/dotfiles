8996d37d56b2a4b912db5a0e424dbee99bde8450
loicgasser <loicgasser4@gmail.com>
2018-08-29 00:54:03 -0400

    Remove hack and cleanup

--- .bash_profile
+++ .bash_profile
@@ -1,6 +1,35 @@
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
+if [ -f /etc/bash_completion ]; then
+  . /etc/bash_completion
+fi
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi
+
+### Export variables
+export LESS='-q'
+export EDITOR=vim
+export PATH=$HOME/local/bin:$HOME/bin:$PATH
+
+### ls options
+export LS_OPTIONS='--color=auto'
+export HISTCONTROL=ignoredups
+eval `dircolors`
+alias ls='ls $LS_OPTIONS'
+alias l='ls $LS_OPTIONS -Fahl'
+
+### Custom bar
+parse_git_branch()
+{
+  local BRANCH=$(git symbolic-ref HEAD --short 2> /dev/null)
+  if [[ ! -z "$BRANCH" ]]
+  then
+    echo "**($BRANCH)**"
+  fi
+}
+YELLOW='\e[0;33m'     # Yellow
+WHITE='\e[0;37m'      # White
+RESET='\e[0m'         # Text Reset
+export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]${YELLOW}\$(parse_git_branch) ${WHITE}\n$ "
--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-cp -f .gitconfig ~/
-cp -f .tmux.conf ~/
-cp -f .vimrc ~/
-cp -f .bash_profile ~/
-cp -f saveagent.sh ~/.ssh/saveagent
-if [ -f ~/.bashrc ]; then
-    mv ~/.bashrc ~/bashrc
-    cat ~/bashrc .bashrc > ~/.bashrc
-    rm -f ~/bashrc
-else
-    cp -f .bashrc ~/.bashrc
-fi
+echo 'Hi bro, I am gona tweak your home a notch, but not too much :D !!'
+
+BASEDIR=$(dirname "$0")
+
+### Create symlinks for dotgile
+ln -s ${BASEDIR}/.gitconfig ~/
+ln -s ${BASEDIR}/.tmux.conf ~/
+ln -s ${BASEDIR}/.vimrc ~/
+ln -s ${BASEDIR}/.bash_profile ~/
+
+### Create AWS/PostgreSQL related file
 touch ~/.pgpass
 chmod go-rwx ~/.pgpass
 touch ~/.boto
@@ -19,13 +18,18 @@ chmod go-rwx ~/.boto
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 chmod go-rwx ~/.aws/credentials
-curl -k https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+if [ ! ~/.git-completion.bash ]; then
+  curl -k https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+fi
 
+### Vim tweaks with pathogen and synastic
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/syntastic.git
-
-npm install jshint
+if [ ! -d ~/.vim/bundle/syntastic ]; then
+  cd ~/.vim/bundle
+  git clone https://github.com/scrooloose/syntastic.git
+  npm install jshint
+fi
